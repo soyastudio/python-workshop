134a7464936f0bca17105c794bdaee20275aa651
Wen Qun <wenqun.soya@gmail.com>
2026-01-30 15:15:23 -0800

    python workshop rename

--- main.py
+++
@@ -1,9 +0,0 @@
-import ollama
-
-response = ollama.chat(model='deepseek-r1:8b', messages=[
-  {
-    'role': 'user',
-    'content': 'Write a Python function to sort a list.',
-  },
-])
-print(response['message']['content'])
